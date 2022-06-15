3a4906e5c476f605352faee6fd52011481bbc534
Shashank <shashankmish27@gmail.com>
2022-06-15 19:19:26 +0530

    Finished Basic Calc, Design Pending

--- config.js
+++ config.js
@@ -1,80 +1,97 @@
 mathString = "";
-//node which has all operations selected
-let operations = document.querySelectorAll(".opr");
-//node which has screen selected
-let display = document.querySelector("#calculation-string");
+
+//node which has questionString selected
+let qString = document.querySelector("#calculation-string");
 //node which has answer selected
 let answer = document.querySelector("#answer");
 
-let buttons = document.querySelectorAll("button");
-buttons.forEach(button => {
-    // !!!!!it doesnt make sense to code a generic event listener function, much better to code one for each pad -> numberpad, operations and equals.
-    button.addEventListener('click', () => {
-        //register the button click, identify which type of button it is
-        if (button.className == "num") {
-            operations.forEach(operate => operate.disabled = false)
-            mathString = mathString + button.id;
-        }
-        else if (button.className == "spc") {
-            if (button.id == "clc") {
-                mathString = "";
-                operations.forEach(operate => operate.disabled = false);
-            }
-            else if (button.id == "del") {
-                mathString = mathString.slice(0, mathString.length - 1)
-                //detect the most recent character in the string remaining to determine if enable operations
-                lastChar = mathString.slice(-1);
-                console.log(lastChar);
-                if (lastChar == "+" || lastChar == "-" || lastChar == "*" || lastChar == "/") {
-                    //disable the operations
-                    operations.forEach(operate => operate.disabled = true)
-                }
-                else
-                    operations.forEach(operate => operate.disabled = false);
-            }
-        }
-        else {
-            if (button.id == "=") {
-                // while (RemainingOperators) 
-                mathString = mathString + button.id;
-                Evaluate();
-            }
-            //disable operators
+//operationpad event handler
+let operations = document.querySelectorAll(".opr");
+operations.forEach(operator => {
+    operator.addEventListener('click', () => {
+        if (operator.id != "eql") {  //handled seperately, grouped up with operators to disable all with ease
+            mathString = mathString + operator.id;
+            //disable all operations
             operations.forEach(operate => operate.disabled = true)
-            mathString = mathString + button.id;
+            qString.textContent = mathString;
         }
-        //display string on screen
-        display.textContent = mathString;
     })
+})
+
+//numberpad event handler
+let numbers = document.querySelectorAll(".num");
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        //enable all operations
+        operations.forEach(operate => operate.disabled = false)
+        mathString = mathString + number.id;
+        qString.textContent = mathString;
+    })
+})
+
+//specialkeys event handler
+let delButton = document.querySelector("#del");
+delButton.addEventListener('click', () => {
+    mathString = mathString.slice(0, mathString.length - 1)
+    //detect the most recent character in the string remaining to determine if enable operations
+    lastChar = mathString.slice(-1);
+    console.log(lastChar);
+    numbers.forEach(number => number.disabled = false);
+    if (lastChar == "+" || lastChar == "-" || lastChar == "*" || lastChar == "/") {
+        //disable the operations
+        operations.forEach(operate => operate.disabled = true)
+    }
+    else
+        operations.forEach(operate => operate.disabled = false);
+    qString.textContent = mathString;
+})
+
+let clearButton = document.querySelector("#clr");
+clearButton.addEventListener('click', () => {
+    mathString = "";
+    operations.forEach(operate => operate.disabled = false);
+    qString.textContent = mathString;
+    answer.textContent = "";
+    numbers.forEach(number => number.disabled = false);
+})
+
+// equals button handler
+let equalButton = document.querySelector("#eql");
+equalButton.addEventListener('click', () => {
+    mathString = mathString + "=";
+    qString.textContent = mathString;
+    Evaluate(mathString);
+    numbers.forEach(number => number.disabled = true);
+    operations.forEach(operate => operate.disabled = true);
 });
 
-function Evaluate() {
+function Evaluate(calculation) {
     //evaluates the mathString
     //55-6*2/6+13=
     //55+6=
     var operator = 0;
-    console.log(mathString);
-    for (let index = 0; index < mathString.length; index++) {
-        if (mathString[index] == "+" || mathString[index] == "-" || mathString[index] == "*" || mathString[index] == "/") {
-            var num1 = Number(mathString.slice(0, index));
+    console.log(calculation);
+    for (let index = 0; index < calculation.length; index++) {
+        if (OperatorFind(calculation[index])) {
+            var num1 = Number(calculation.slice(0, index));
             console.log("number1 = " + num1);
             console.log("index = " + index);
-            if (mathString[index] == "+") operator = 1;
-            if (mathString[index] == "-") operator = 2;
-            if (mathString[index] == "*") operator = 3;
-            if (mathString[index] == "/") operator = 4;
-            console.log(operator);
+            operator = OperatorFind(calculation[index]);
+            if (operator == 5){
+                //if operator is =
+                answer.textContent = calculation.slice(0, calculation.length - 1);
+                return;
+            }
+            console.log(operator + " is the operator from 1st loop");
             //problem occurs after this point. no immediate solution !?
-            for (let index2 = index+1; index2 < mathString.length; index2++) {
+            for (let index2 = index + 1; index2 < calculation.length; index2++) {
                 console.log("index rn -> " + index2);
-                if (mathString[index2] == "+" || mathString[index2] == "-" || mathString[index2] == "*" || mathString[index2] == "/") {
-                    var num2 = Number(mathString.slice(index + 1, index2));
+                if (OperatorFind(calculation[index2])) {
+                    var num2 = Number(calculation.slice(index+1, index2));
                     console.log(num2);
-                    mathString = Operate(num1, num2, operator) + mathString.slice(index2, mathString.length);
+                    calculation = Operate(num1, num2, operator) + calculation.slice(index2, calculation.length);
+                    Evaluate(calculation);
                     return;
-                }
-                else if (mathString[index] == "=") {
-                    answer.textContent = mathString.slice(0,mathString-1);
                 }
             }
         }
@@ -82,28 +99,23 @@ function Evaluate() {
     //if string still contains operators, run the evaluate function again ---> done by operate function
 }
 
-function Operate(number1, number2, operator){
+function OperatorFind(operator){
+    if (operator == "+") return 1;
+    else if (operator == "-") return 2;
+    else if (operator == "*") return 3;
+    else if (operator == "/") return 4;
+    else if (operator == "=") return 5;
+    else return 0;
+}
+
+function Operate(number1, number2, operator) {
     if (operator == 1) return number1 + number2;
     if (operator == 2) return number1 - number2;
-    if (operator == 3) return number1 * number2;
+    if (operator == 3) return number1 * number2; //division does not work
     if (operator == 4) {
-        try {
-            number1 / number2;
-            if (!number2)
-            throw new Error("Invalid Division");
-        }
-
-        catch(e) {
-            answer.textContent = "Error";
-        }
-
+        if (number2) return (number1/number2).toFixed(3);
+        answer.textContent = "ERROR";
+        numbers.forEach(number => number.disabled = true);
     }
-}
-
-function RemainingOperators (){
-    for (let index = 0; index < mathString.length; index++) {
-        if (mathString[index] == "+" || mathString[index] == "-" || mathString[index] == "*" || mathString[index] == "/" || mathString[index] == "=")
-            return true;
-        else return false;
-}
+    if (operator == 5) return true
 }
