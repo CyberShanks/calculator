6ae87535bb9fcc057c70448f08cace5c485706c7
Shashank <shashankmish27@gmail.com>
2022-06-15 17:45:55 +0530

    One event listener for all

--- config.js
+++ config.js
@@ -2,10 +2,13 @@ mathString = "";
 //node which has all operations selected
 let operations = document.querySelectorAll(".opr");
 //node which has screen selected
-let display = document.querySelector("#screen");
+let display = document.querySelector("#calculation-string");
+//node which has answer selected
+let answer = document.querySelector("#answer");
 
 let buttons = document.querySelectorAll("button");
 buttons.forEach(button => {
+    // !!!!!it doesnt make sense to code a generic event listener function, much better to code one for each pad -> numberpad, operations and equals.
     button.addEventListener('click', () => {
         //register the button click, identify which type of button it is
         if (button.className == "num") {
@@ -31,7 +34,11 @@ buttons.forEach(button => {
             }
         }
         else {
-            if (button.id == "eql") evaluate();
+            if (button.id == "=") {
+                // while (RemainingOperators) 
+                mathString = mathString + button.id;
+                Evaluate();
+            }
             //disable operators
             operations.forEach(operate => operate.disabled = true)
             mathString = mathString + button.id;
@@ -41,6 +48,62 @@ buttons.forEach(button => {
     })
 });
 
-function evaluate() {
-    //evaluates the string of calculation
+function Evaluate() {
+    //evaluates the mathString
+    //55-6*2/6+13=
+    //55+6=
+    var operator = 0;
+    console.log(mathString);
+    for (let index = 0; index < mathString.length; index++) {
+        if (mathString[index] == "+" || mathString[index] == "-" || mathString[index] == "*" || mathString[index] == "/") {
+            var num1 = Number(mathString.slice(0, index));
+            console.log("number1 = " + num1);
+            console.log("index = " + index);
+            if (mathString[index] == "+") operator = 1;
+            if (mathString[index] == "-") operator = 2;
+            if (mathString[index] == "*") operator = 3;
+            if (mathString[index] == "/") operator = 4;
+            console.log(operator);
+            //problem occurs after this point. no immediate solution !?
+            for (let index2 = index+1; index2 < mathString.length; index2++) {
+                console.log("index rn -> " + index2);
+                if (mathString[index2] == "+" || mathString[index2] == "-" || mathString[index2] == "*" || mathString[index2] == "/") {
+                    var num2 = Number(mathString.slice(index + 1, index2));
+                    console.log(num2);
+                    mathString = Operate(num1, num2, operator) + mathString.slice(index2, mathString.length);
+                    return;
+                }
+                else if (mathString[index] == "=") {
+                    answer.textContent = mathString.slice(0,mathString-1);
+                }
+            }
+        }
+    }
+    //if string still contains operators, run the evaluate function again ---> done by operate function
+}
+
+function Operate(number1, number2, operator){
+    if (operator == 1) return number1 + number2;
+    if (operator == 2) return number1 - number2;
+    if (operator == 3) return number1 * number2;
+    if (operator == 4) {
+        try {
+            number1 / number2;
+            if (!number2)
+            throw new Error("Invalid Division");
+        }
+
+        catch(e) {
+            answer.textContent = "Error";
+        }
+
+    }
+}
+
+function RemainingOperators (){
+    for (let index = 0; index < mathString.length; index++) {
+        if (mathString[index] == "+" || mathString[index] == "-" || mathString[index] == "*" || mathString[index] == "/" || mathString[index] == "=")
+            return true;
+        else return false;
+}
 }
